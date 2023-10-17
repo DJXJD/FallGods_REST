17a4c131a401a7a1a53cb38ce110bec8b7584a64
DJXJD <djxjd5713@gmail.com>
2023-10-16 23:34:41 -0400

    Moved MinigameData to its own class from Player inner class

--- src/main/java/dev/djxjd/fallgods/beans/Player.java
+++ src/main/java/dev/djxjd/fallgods/beans/Player.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import dev.djxjd.fallgods.beans.wrappers.MinigameData;
 import dev.djxjd.fallgods.services.listeners.PlayerListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,7 +15,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Transient;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -59,22 +59,5 @@ public class Player extends DBEntity<Player> {
 	@Transient
 	@Singular("mapData")
 	private Map<Minigame, MinigameData> mapData;
-	
-	@Data
-	@NoArgsConstructor
-	public class MinigameData {
-		private int plays;
-		private float occurrence;
-		private int qualifications;
-		private float qualificationRate;
-		private int eliminations;
-		private float eliminationRate;
-		private int finishes;
-		private float finishRate;
-		private int fumbles;
-		private float fumbleRate;
-		private int mvps;
-		private float mvpRate;
-	}
 	
 }
